bf349fe5a930243181d14a3f3ba3a7b91bd38a80
Nelson <xnelsonvsx@hotmail.com>
2021-01-20 21:31:59 -0500

    condicionales.js

---
+++ condicionales.js
@@ -0,0 +1,23 @@
+const persona = {
+    nombre: 'Billie',
+    apellido: 'Eillish',
+    edad: 17
+}
+
+const persona = {
+    nombre: 'Nelson',
+    apellido: 'Varela',
+    edad: 27
+}
+
+if (persona.edad < 18) {
+    console.log('No puede tomar cerveza')
+} else {
+    console.log('Sí puede tomar cerveza')
+}
+
+if (persona2.edad < 18) {
+    console.log('No puede tomar cerveza')
+} else {
+    console.log('Sí puede tomar cerveza')
+}
